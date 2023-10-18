305cddbeeefe80d2da00d9b9e7a8b98241bea37d
FaheemBD <127047777+FaheemBD@users.noreply.github.com>
2023-10-18 12:44:46 +0600

    Add files via upload

---
+++ Function_Color.java
@@ -0,0 +1,48 @@
+
+
+import java.awt.Color;
+
+public class Function_Color {
+	GUI gui;
+	
+
+	public Function_Color(GUI gui) {
+		this.gui=gui;
+	}
+
+
+	public void changeColor(String color) {
+		switch (color) {
+		case "White":
+			gui.window.getContentPane().setBackground(Color.white);
+			gui.textArea.setBackground(Color.white);
+			gui.textArea.setForeground(Color.black);
+			break;
+
+		case "Blue":
+			gui.window.getContentPane().setBackground(Color.blue);
+			gui.textArea.setBackground(Color.blue);
+			gui.textArea.setForeground(Color.white);
+			break;
+			
+		case "Red":
+			gui.window.getContentPane().setBackground(Color.red);
+			gui.textArea.setBackground(Color.red);
+			gui.textArea.setForeground(Color.black);
+			break;
+
+		case "Black":
+			gui.window.getContentPane().setBackground(Color.black);
+			gui.textArea.setBackground(Color.black);
+			gui.textArea.setForeground(Color.white);
+			break;
+		case "Green":
+			gui.window.getContentPane().setBackground(Color.green);
+			gui.textArea.setBackground(Color.green);
+			gui.textArea.setForeground(Color.black);
+			break;
+		}
+		
+	}
+
+}
